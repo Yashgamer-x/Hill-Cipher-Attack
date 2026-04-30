b258c34b6eeee7462d609c31458b9ce7b65d0f3c
Yashgamer-x <yashdesai121212@gmail.com>
2026-04-30 00:58:01 -0400

    Completed Hillcipher encryption and decryption

--- src/main/java/org/yashgamerx/HillCipher.java
+++ src/main/java/org/yashgamerx/HillCipher.java
@@ -5,15 +5,17 @@ import lombok.extern.java.Log;
 @Log
 public class HillCipher {
 
-    public static int[][] encrypt(int[][] matrix){
-        var determinant = DeterminantCalculator.determinant(matrix);
-        var moddedDeterminant = ModCalculator.calculate(determinant, 26);
-        var inverseModdedDeterminant = ModCalculator.modInverse(moddedDeterminant,26);
-        return MatrixCalculator.moddedScalarMultiplication(matrix, inverseModdedDeterminant, 26);
+    public static int[] encrypt(int[] plainText, int[][] key){
+        return MatrixCalculator.moddedMultiplication(plainText, key, 26);
     }
 
-    public static void decrypt(int[][] matrix){
-
+    public static int[] decrypt(int[] cipherMatrix, int[][] key) {
+        var det = DeterminantCalculator.determinant(key);
+        var moddedDeterminant = ModCalculator.calculate(det, 26);
+        var inverseModdedDeterminant = ModCalculator.modInverse(moddedDeterminant, 26);
+        var adjunctKeyMatrix = MatrixCalculator.adjunct(key);
+        var inverseKeyMatrix = MatrixCalculator.moddedScalarMultiplication(adjunctKeyMatrix, inverseModdedDeterminant, 26);
+        return MatrixCalculator.moddedMultiplication(cipherMatrix, inverseKeyMatrix, 26);
     }
 
 }
--- src/main/java/org/yashgamerx/MatrixCalculator.java
+++ src/main/java/org/yashgamerx/MatrixCalculator.java
@@ -16,4 +16,26 @@ public class MatrixCalculator {
 
         return newMatrix;
     }
+
+    public static int[][] adjunct(int[][] matrix){
+        var newMatrix = new int[matrix.length][matrix.length];
+        newMatrix[0][0] = matrix[1][1];
+        newMatrix[1][1] = matrix[0][0];
+        newMatrix[0][1] = matrix[1][0];
+        newMatrix[1][0] = matrix[0][1];
+        return newMatrix;
+    }
+
+    public static int[] moddedMultiplication(
+            int[] matrix1,
+            int[][] matrix2,
+            int mod)
+    {
+        var newMatrix = new int[2];
+        newMatrix[0] = (matrix2[0][0]*matrix1[0] +
+                matrix2[1][0]*matrix1[1]) % mod;
+        newMatrix[1] = (matrix2[0][1]*matrix1[0] +
+                matrix2[1][1]*matrix1[1]) % mod;
+        return newMatrix;
+    }
 }
