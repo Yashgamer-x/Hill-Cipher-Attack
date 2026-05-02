0657a3e144cd9659956dea3c7797a46fcf156c21
Yashgamer-x <yashdesai121212@gmail.com>
2026-05-02 11:11:08 -0400

    Commented HillCipher

--- src/main/java/org/yashgamerx/HillCipher.java
+++ src/main/java/org/yashgamerx/HillCipher.java
@@ -5,10 +5,42 @@ import lombok.extern.java.Log;
 @Log
 public class HillCipher {
 
+    /// Encrypts the plainText into cipherText
+    /// @param plainText A 2x1 matrix
+    /// e.g. ```java
+    /// int[] plainText = {4, 7}
+    /// ```
+    /// @param key A 2x2 matrix.
+    /// e.g. ```java
+    /// int[][] key = {
+    ///     {1, 2},
+    ///     {3, 4}
+    /// };
+    /// ```
+    /// @return CipherText.
+    /// e.g. ```java
+    /// int[] cipherText = {2, 1}
+    /// ```
     public static int[] encrypt(int[] plainText, int[][] key){
         return MatrixCalculator.moddedMultiplication(plainText, key, 26);
     }
 
+    /// Decrypts the cipherText into plainText
+    /// @param cipherMatrix A 2x1 matrix
+    /// e.g. ```java
+    /// int[] cipherMatrix = {2, 1}
+    /// ```
+    /// @param key A 2x2 matrix.
+    /// e.g. ```java
+    /// int[][] key = {
+    ///     {1, 2},
+    ///     {3, 4}
+    /// };
+    /// ```
+    /// @return decipheredText.
+    /// e.g. ```java
+    /// int[] decipheredText = {4, 7}
+    /// ```
     public static int[] decrypt(int[] cipherMatrix, int[][] key) {
         var inverseKeyMatrix = MatrixCalculator.moddedInverseMatrix(key);
         return MatrixCalculator.moddedMultiplication(cipherMatrix, inverseKeyMatrix, 26);
